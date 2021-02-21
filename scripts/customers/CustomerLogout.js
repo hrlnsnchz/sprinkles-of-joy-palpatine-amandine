56910d796796fbd400953aeb526cf6be7e7d2b42
hrlnsnchz <hrlnsnchz@gmail.com>
2021-02-20 22:19:30 -0600

    Added a comment and deleted ghost code for logout button.

--- scripts/customers/CustomerLogout.js
+++ scripts/customers/CustomerLogout.js
@@ -1,8 +1,6 @@
-import { CustomerNav } from "./CustomerNav.js"
-import { LoginForm } from "./LoginForm.js"
-
 const eventHub = document.querySelector("#container")
 
+// Listening for logout button click. Then clears sesssion storage and refreshes page.
 eventHub.addEventListener("logoutUser", event => {
     sessionStorage.clear()
     location.reload()

6f31fdfb5387fa83b70aefe94a9abe3af5a7fcf5
hrlnsnchz <hrlnsnchz@gmail.com>
2021-02-23 15:59:48 -0600

    Order history feature work in progress

--- scripts/orders/Order.js
+++ scripts/orders/Order.js
@@ -1,8 +1,31 @@
+
+
+const contentTarget = document.querySelector(".prods")
+
 export const Order = (customerOrder) => {
   return `
     <div class="order">
       <p>${new Date(customerOrder.timestamp).toLocaleString('en-US')}</p>
+      <p class="prods"></p>
       <p>${customerOrder.status.label}</p>
     </div>
   `
+}
+
+export const OrderProducts = (customerOrder, productsArray) => {
+  contentTarget.innerHTML = `
+  <p>${orderProductsFilter(customerOrder, orderProducts)}</p>
+  <p>$${totalCost.toFixed(2)}</p>`
+  orderProductsFilter(ordersArray, productsArray)
+}
+
+
+
+let totalCost = 0
+export const orderProductsFilter = (customerOrdersArray, orderProductsArray) => {
+  const filteredProduct = orderProductsArray.filter(products => products.orderId === customerOrdersArray.id)
+  for (const product of filteredProduct) {
+    totalCost += product.price
+    return filteredProduct
+  }
 }
--- scripts/orders/OrderList.js
+++ scripts/orders/OrderList.js
@@ -1,26 +1,33 @@
 import { authHelper } from "../auth/authHelper.js"
 import { getCustomer } from "../customers/CustomerProvider.js"
-import { Order } from "./Order.js"
+import { Order, orderProductsFilter, OrderProducts} from "./Order.js"
+import { getOrderProducts, useOrderProducts } from "./OrderProductProvider.js"
 import { getOrders, useOrders } from "./OrderProvider.js"
+
+
 
 const eventHub = document.querySelector("#container")
 const contentContainer = document.querySelector(".userOrders")
 
 let customerOrders = []
+let orderProducts = []
 
 export const OrderList = () => {
   if (authHelper.isUserLoggedIn()) {
 
     getOrders()
+    .then(getOrderProducts)
       .then(() => {
         customerOrders = useOrders()
+        orderProducts = useOrderProducts()
         render()
       })
   }
 }
 
 const render = () => {
-  const ordersHtmlRepresentation = customerOrders.map(order => Order(order)).join("")
+  const ordersHtmlRepresentation = customerOrders.map(order=> Order(order)).join("")
+  const productOrderHTMLRepresentation = orderProducts.filter(() => OrderProducts(customerOrders, orderProducts)).join("")
 
   contentContainer.innerHTML = `
   <div id="orders__modal" class="modal--parent">
@@ -29,6 +36,7 @@ const render = () => {
         <div>
         <h5>Ordered on</h5>
         ${ordersHtmlRepresentation}
+        ${productOrderHTMLRepresentation}
         </div>
         <button id="modal--close">Close</button>
         </div>
